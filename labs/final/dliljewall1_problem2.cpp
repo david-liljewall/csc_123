420335cfbf77db022103e1e8c89954d845471837
david-liljewall <davidliljewall96@gmail.com>
2020-05-10 21:41:00 -0500

    edits

--- labs/final/dliljewall1_problem2.cpp
+++ labs/final/dliljewall1_problem2.cpp
@@ -20,9 +20,11 @@ using namespace std;
 /*                             STRUCT DECLARATIONS                            */
 /* -------------------------------------------------------------------------- */
 struct Pot {
-    // 
+    // struct Pot member variables defined below:
+
     int plant_days; // how many days since a monster plant moved in (cell was filled)
     bool is_planted; // tells whether or not pot has a monster plant in it
+
 };
 
 
@@ -34,14 +36,16 @@ void initialize_pots(Pot axis[], int& pots_n, int& monsters_n);
     // Based on user-input for # of monster plants to start with, place them 
     // randomly along the line of pots.
 
-void populate_pots(Pot axis[], int& pots_n, int& monsters_n);
+void monster_duplication_edge(Pot axis[], int& pots_n, int& monsters_n);
+    // Duplicates an existing potted monster plant to an adjacent empty pot.
+
+void monster_duplication_middle(Pot axis[], int& pots_n, int& monsters_n);
     //
 
-void update_pots(Pot axis[], int& pots_n, int& monsters_n);
-    // Updates the line of pots after each day, whereby the monster plants can duplicate
-    // to adjacent pots IF they are already empty.
+void update_pots(Pot axis[], int& pots_n);
+    // Updates the line of pots after each day
 
-void display_pots(Pot axis[], int& pots_n, int& monsters_n, int& days);
+void display_pots(Pot axis[], int& pots_n, int& days);
     // Displays the line of pots for a given day, showing whether a pot is filled with 
     // a monster plant, and the # of days since a monster plant moved in.
 
@@ -50,35 +54,54 @@ void display_pots(Pot axis[], int& pots_n, int& monsters_n, int& days);
 /* -------------------------------------------------------------------------- */
 int main() {
 
-    // NOTE: simulation goes until ALL pots are filled.
     srand(unsigned(time(NULL))); // seeds random number generator
 
-    int n_pots(0), n_monsters(0); // tracks # of pots and # of monster plants
-    int n_days(0); // tracks the number of days (for terminal display purposes)
+    int n_pots = 0, n_monsters = 0; // tracks # of pots and # of monster plants
+    int n_days = 0; // tracks the number of days (for terminal display purposes)
 
-    //* create dynamic memory for the game grid:
-    struct Pot axis[n_pots]; // create a horizontal axis[] to be filled with monsters thru simulation 
+
+    // create dynamic memory for the simulation axis:
+    struct Pot *grid; // create struct object pointer which points to dynamic 1D array which defines the 1D grid of the simulation
 
     cout << " How many pots do you want to start with? (maximum of 32k)" << endl;
     cin >> n_pots;
-    cout << " How many monster plants to start?" << endl;
-    cin >> n_monsters;
 
-    initialize_pots(axis, n_pots, n_monsters);
-    populate_pots(axis, n_pots, n_monsters);
+    grid = new Pot[n_pots]; // initialize a dynamic array grid[]
+
     
-    
-    // NOTE: Simulation starts below:
-    do {
-        display_pots(axis, n_pots, n_monsters, n_days);
+    // continue to simulation only if n_pots is less than defined maximum:
+    if (n_pots > MAX_POTS) {
+        cout << " # of pots entered is larger than 32k... exiting simulation " << endl;
+        exit(1);
+    }
+    else {
+        cout << " How many monster plants to start?" << endl;
+        cin >> n_monsters;
 
+        cout << " << Simulation Start >> " << endl;
+        display_pots(grid, n_pots, n_days);        
+        initialize_pots(grid, n_pots, n_monsters);
+        n_days++;
+        update_pots(grid, n_pots); // update to correct number of days before entering simulation
+        display_pots(grid, n_pots, n_days);
 
-        n_days++; // increment # of days
+       
+        do {
+            n_days++; // increment # of days
+            monster_duplication_middle(grid, n_pots, n_monsters);
+            monster_duplication_edge(grid, n_pots, n_monsters);
+            update_pots(grid, n_pots);
+            display_pots(grid, n_pots, n_days);
 
-    } while (n_monsters < n_pots);
+        } while (n_monsters <= n_pots);
 
-    getchar();
-    
+        // getchar();
+    }
+
+    // delete dynamic array grid[] and set to NULL to prevent memory leaks:
+    delete[] grid;
+    grid = NULL;
+
     return 0;
 }
 
@@ -87,50 +110,117 @@ int main() {
 /* -------------------------------------------------------------------------- */
 
 void initialize_pots(Pot axis[], int& pots_n, int& monsters_n) {
-
+    
+    int monster_number = 0;
+    int i_rand = 0;
+    
+    // initialize struct member variables before simulation starts:
     for (int i = 0; i < pots_n; i++) {
+
         axis[i].is_planted = false;
         axis[i].plant_days = 0;
+
     }
 
-}
-
-void populate_pots(Pot axis[], int& pots_n, int& monsters_n) {
-    int monster_number = 0; // initialize number of potted monster plants
-    int x_rand; // randomized cubicle x index
-
-    // populate axis with monster plants:
+    i_rand = rand() % pots_n; // i_rand between 0 and number of pots chosen by user
+    // populate axis with user-entered number of monster plants:
     while (monster_number < monsters_n) {
-        x_rand = rand() % pots_n + 1;
+        
 
-        // if not filled with monster plant already
-        if (!axis[x_rand].is_planted) {
+        if (!axis[i_rand].is_planted) {
 
-            axis[x_rand].is_planted = true;
+            axis[i_rand].is_planted = true;
+            monster_number++; // tracks how many monsters have been populated
+
         }
-        monster_number++;
+
+        i_rand = rand() % pots_n; // set new i_rand
+
     }
+
 }
 
-void update_pots(Pot axis[], int& pots_n, int& monsters_n) {
-    int monster_number;
+void monster_duplication_edge(Pot axis[], int& pots_n, int& monsters_n) {
 
+    int i = 0; // left-most pot index
+    if (axis[i].is_planted) {
+        // i == 0 and there exists a plant, check adjacent pot (to the right)
+        if (!axis[i + 1].is_planted) {
+        
+        axis[i + 1].is_planted = true;
+        monsters_n++; // increment # of monster plants
+
+        }
+            
+    }
+
+    i = pots_n - 1; // right-most pot index
+    if (axis[i].is_planted) {
+        // i == 0 and there exists a plant, check adjacent pot (to the right)
+        if (!axis[i - 1].is_planted) {
+        
+        axis[i - 1].is_planted = true;
+        monsters_n++; // increment # of monster plants
+
+        }
+            
+    }
+    
+}
+
+void monster_duplication_middle(Pot axis[], int& pots_n, int& monsters_n) {
+    
+    int i = 1; // start at first non-edge pot index
+    while (i < pots_n) {
+        // i == 1,2,..pots_n
+        if (axis[i].is_planted) {
+
+            if (!axis[i - 1].is_planted) {
+                
+                axis[i - 1].is_planted = true;
+                monsters_n++; // increment # of monster plants
+            }
+            if (!axis[i + 1].is_planted) {
+
+                axis[i + 1].is_planted = true;
+                monsters_n++; // increment # of monster plants
+            }
+
+            i+=2;
+        }
+        else {
+
+            i++;
+        }
+
+    }
+}
+void update_pots(Pot axis[], int& pots_n) {
+    
     for (int i = 0; i < pots_n; i++) {
         
         if (axis[i].is_planted) {
             
-            axis[i].plant_days += 1;
+            axis[i].plant_days += 1; // incremement # of plant days on planted pots
         }
-        else if(!axis[i].is_planted) {
+    }
+}
 
+void display_pots(Pot axis[], int& pots_n, int& days) {
+    cout << " << Day " << days << ": " << endl;
+
+    for (int i = 0; i < pots_n; i++) {
+
+        if (axis[i].is_planted) {
+            cout << "\t[ " << axis[i].plant_days << " ]";
+        }
+        else {
+            cout << "\t[ " << "-" << " ]";
         }
 
     }
 
-}
-
-void display_pots(Pot axis[], int& pots_n, int& monsters_n, int& days) {
-    cout << " << Day " << days << " : " << endl;
+    cout << endl; // add extra whitespace
 
 
 }
