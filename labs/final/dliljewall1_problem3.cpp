ec3879efe884b663efbdd84f05e30493ea4761ca
david-liljewall <davidliljewall96@gmail.com>
2020-06-29 13:31:08 -0500

    added folder

--- labs/final/dliljewall1_problem3.cpp
+++ labs/final/dliljewall1_problem3.cpp
@@ -43,7 +43,6 @@ class Movie {
         }
 };
 
-
 class List {
     // contains a list/vector of objects of class-type Movie
 
@@ -71,7 +70,7 @@ public:
         // user enters name of movie and searches the list
     void display_list(Movie& movie);
         // user picks whether to display the viewing list BY NAME or by FAVORITE VALUE (both are sorted)
-    void save_data();
+    void save_data(Movie& movie, string file_name);
         // saves the current viewing list to a file saving each movie information one line at a time:
 	    // 1. Name
 	    // 2. Length
@@ -80,12 +79,11 @@ public:
     void disney_welcome();
         // prints mickey mouse and welcomes user to Disney plus plus
 
+
     void print_vector();
         // NOTE: FOR DEBUGGING ONLY
         // prints the movie_list vector
 
-    void sort_vector();
-        // Bubble Sorts vector movie_list
 };
 
 
@@ -108,12 +106,13 @@ int main() {
         // start looping menu prompt:
         MENU_PROMPT: cout << endl << " << MENU >> " << endl;
 
-        cout << endl << " Which option would you like to perform? " << endl << endl
+        cout << endl << " Choose from the menu option below: " << endl << endl
         << " 1) Add a Movie to Your List" << endl
         << " 2) Remove a Movie from Your List" << endl
         << " 3) Search for a Movie in Your List" << endl
-        << " 4) Save Your List Data" << endl
-        << " 5) Exit Disney++" << endl << endl;
+        << " 4) Display Your List" << endl
+        << " 5) Save Your List Data" << endl
+        << " 6) Exit Disney++" << endl << endl;
         cin >> option;
 
         if (!cin.fail()) {
@@ -121,29 +120,34 @@ int main() {
             case 1:
                 // add movie to list
                 list.add_movie(film);
-                list.print_vector(); // for debugging 
+                // list.print_vector(); // for debugging 
                 break;
             
             case 2: 
                 // remove movie from list
                 list.remove_movie(film);
-                list.print_vector(); // for debugging 
+                // list.print_vector(); // for debugging 
                 break;
             
             case 3: 
                 // search for movie in list
                 list.search_movie(film);
                 break;
-
-            // case 4:
-            //     // save list data
-            //     list.save_data();
-            //     break;
+            
+            case 4:
+                // display sorted movie list
+                list.display_list(film);
+                break;
 
             case 5:
+                // save list data
+                list.save_data(film, "Viewing_List.txt");
+                break;
+
+            case 6:
                 // exit program
-                cout << " << You have successfully exited Disney++." << endl
-                << " Thanks for using our service, and we hope to see you back soon!" << endl << endl;
+                cout << " << You have successfully exited Disney++ >>" << endl
+                << " << Thanks for using our service, and we hope to see you back soon! >>" << endl << endl;
                 break;
 
             default:
@@ -161,7 +165,7 @@ int main() {
 
         }
 
-    } while (option < 5);
+    } while (option < 6);
 
     return 0;
 }
@@ -189,10 +193,11 @@ void List::add_movie(Movie& movie) {
 
 
         cout << endl << " --> " << movie.name << " <-- " << " has been added to your list! " 
-        << " It has a runtime of " << movie.length << " minutes" << endl;
+        << " It has a runtime of " << movie.length << " minutes." << endl;
 
-        cout << " Would you like to add an additional movie to your viewing list? (type y or n)" << endl;
+        cout << " Would you like to add an additional movie to your viewing list? (type y or n) ";
         cin >> input;
+
         if (input == "y") {
             // do nothing, repeat prompt loop
         }
@@ -202,7 +207,7 @@ void List::add_movie(Movie& movie) {
 
     } while (input != "n");
 
-    cout << endl; // add extra whitespace
+    cout << endl << endl; // add extra whitespace
         
 }
 
@@ -219,20 +224,30 @@ void List::remove_movie(Movie& movie) {
         getline(cin >> ws, movie.name); // getline(cin >> ws variable) allows for spaces to be entered in terminal
         
         // remove name from list
+        bool removed = false; // tracks if movie was removed or not
+
         for (int i = 0; i < movie_list.size(); i++) {
+
             if (movie_list[i].name.compare(movie.name) == 0) {
                 // if user input matches existing string in movie_list vector:
-                movie_list.erase(movie_list.begin() + i); // deletes the ith element; the element that matches the searched movie name
+                movie_list.erase(movie_list.begin() + i); // deletes the ith element (which matches movie.name)
+                removed = true;
             }
-            else {
-                cout << movie.name << " is not in your list. Please try again" << endl;
-            }
+
         }
 
-        cout << " --> " << movie.name << " <-- " << " has been removed from your list!" << endl;
+        if (removed) {     
 
-        cout << " Would you like to remove any additional movies from your viewing list? (type y or n)" << endl;
+            cout << " --> " << movie.name << " <-- " << " has been removed from your list!" << endl;
+        }
+        else {
+
+            cout << " The movie title you want to remove is not in your list..." << endl << endl;
+        }
+            
+        cout << " Would you like to remove any additional movies from your viewing list? (type y or n) ";
         cin >> input;
+
         if (input == "y") {
             // do nothing, repeat prompt loop
         }
@@ -242,7 +257,7 @@ void List::remove_movie(Movie& movie) {
 
     } while (input != "n");
 
-    cout << endl; // add extra whitespace
+    cout << endl << endl; // add extra whitespace
 }
 
 void List::search_movie(Movie& movie) {
@@ -258,17 +273,24 @@ void List::search_movie(Movie& movie) {
         getline(cin >> ws, movie.name); // getline(cin >> ws variable) allows for spaces to be entered in terminal
         
         // search for title in list
+        bool searched = false; // tracks if movie was removed or not
+
         for (int i = 0; i < movie_list.size(); i++) {
             if (movie_list[i].name.compare(movie.name) == 0) {
                 // if user input matches existing string in movie_list vector:
 
-                movie.favorite++; // incremement favorite value
+                movie_list[i].favorite++; // incremement favorite value of specific movie that was searched
+                searched = true;
 
-                cout << " The movie you searched is: " << " --> " << movie.name << " <-- " << " and it is in your list." << endl;
             }
-            else {
-                cout << " --> " << movie.name << " <--" << " is not in your list. Please try again" << endl;
-            }
+    
+        }
+
+        if (searched) {
+            cout << " The movie you searched is: " << " --> " << movie.name << " <-- " << " and it is in your list." << endl;
+        }
+        else {
+            cout << " The movie title you searched is not in your list..." << endl << endl;
         }
 
         cout << " Would you like to search any additional movies from your viewing list? (type y or n)" << endl;
@@ -282,32 +304,82 @@ void List::search_movie(Movie& movie) {
 
     } while (input != "n");
 
-    cout << endl; // add extra whitespace
+    cout << endl << endl; // add extra whitespace
 
 }
 
 void List::display_list(Movie& movie) {
 
-    string input; // stores user-input from terminal
-    int option;
+    string input; 
+    int option; 
 
     cout << " << You have selected the DISPLAY LIST menu option " << endl << endl;
 
     do { 
-        
+
+        cout << " Please select an option below for how you would like to view viewing list: " << endl;
+
         cout << " 1) Display your viewing list, sorted by --> MOVIE TITLE" <<
         endl << " 2) Display your viewing list, sorted by --> RUNTIME" << endl;
         cin >> option;
 
         if (option == 1) {
             // display list of movie titles, sorted
+            for (int i = movie_list.size() - 1; i > 0; i--) {
+                // start at 0th element, and bubble the largest number to the right:
+                for (int j = 0; j < i; j++) {
+                    // check if element to the right of current element is smaller.
+                    // if yes, swap 
+                    if (movie_list[j].name > movie_list[j + 1].name) {
+                        // have to sort by name and keep track of runtime, otherwise they don't move together
+                        string temp1 = movie_list[j + 1].name;
+                        int temp2 = movie_list[j + 1].length;
 
+                        movie_list[j + 1].name = movie_list[j].name;
+                        movie_list[j + 1].length = movie_list[j].length;
+
+                        movie_list[j].name = temp1;
+                        movie_list[j].length = temp2;
+
+                    }
+                }
+            }
+
+            cout << " << Viewing List (sorted by title): " << endl;
+            for (int i = 0; i < movie_list.size(); i++) {
+                // print movie list
+                cout << setw(5) << (i + 1) << ": " << movie_list[i].name << " -- " << movie_list[i].length << " mins " << endl;
+            }          
 
 
         }
         else if (option == 2) {
             // display list of movie runtimes, sorted 
+
+            for (int i = movie_list.size() - 1; i > 0; i--) {
+                // start at 0th element, and bubble the largest number to the right:
+                for (int j = 0; j < i; j++) {
+                    // check if element to the right of current element is smaller.
+                    // if yes, swap the numbers:
+                    if (movie_list[j].length > movie_list[j + 1].length) {
+                        // have to sort by name and keep track of runtime, otherwise they don't move together
+                        string temp1 = movie_list[j + 1].name;
+                        int temp2 = movie_list[j + 1].length;
+
+                        movie_list[j + 1].name = movie_list[j].name;
+                        movie_list[j + 1].length = movie_list[j].length;
+
+                        movie_list[j].name = temp1;
+                        movie_list[j].length = temp2;
+                    }
+                }
+            }          
             
+            cout << " << Viewing List (sorted by runtime): " << endl;
+            for (int i = 0; i < movie_list.size(); i++) {
+                // print movie list
+                cout << setw(5) << (i + 1) << ": " << movie_list[i].name << " -- " << movie_list[i].length << " mins " << endl;
+            }      
 
         }
         else {
@@ -316,12 +388,55 @@ void List::display_list(Movie& movie) {
 
         }
 
+        cout << endl <<  " Would you like to view your list again? (type y or n)" << endl;
+        cin >> input;
+        if (input == "y") {
+            // do nothing, repeat prompt loop
+        }
+        else if (input == "n") {
+            break;
+        }
+
+
     } while (input != "n");
 
 }
 
-void List::save_data() {
+void List::save_data(Movie& movie, string file_name) {
+    // create output file with name "file_name", passed by value 
+    // This creates the output file that fills with Viewing List data
+    ofstream output_file(file_name);
 
+    if (!output_file) {
+        cout << "Viewing List output not created properly." << endl;
+    }
+
+    // check if file opened properly
+    if (output_file.is_open()) {
+        
+        for (int i = 0; i < movie_list.size(); i++) {
+            // print:
+            // Movie #n: name
+            // Movie #n: length
+            // Movie #n: favorite
+            output_file << "Movie #" << (i + 1) << ": " 
+            << movie_list[i].name << endl
+            << "Runtime: " << movie_list[i].length << endl
+            << "Favorite: " << movie_list[i].favorite << endl << endl;
+        }
+
+        cout << " --> Your Viewing List has been saved to the file Viewing_List.txt " << endl << endl;
+
+    }
+    else {
+        
+        cout << " Output file did not open properly..." << endl
+        << " Returning to main menu." << endl << endl;
+    }
+
+    output_file.close();
+
+    
 }
 
 
@@ -363,8 +478,4 @@ void List::print_vector() {
 
 }
 
-
-void List::sort_vector() {
-
-}
 
